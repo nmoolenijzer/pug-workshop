aadc19a993a331ade55557ac2b04ad7297af51b3
Nick Moolenijzer <nick@moolenijzer.com>
2017-04-25 14:55:03 -0400

    pug shop

--- options.js
+++ options.js
@@ -7,20 +7,10 @@ exports.sitemap = [
     header: 'Category A',
     links: [
       { name: 'Link A.1', url: '#' },
+      { name: 'Link A.4', url: '#' },
       { name: 'Link A.2', url: '#' },
       { name: 'Link A.3', url: '#' },
-      { name: 'Link A.4', url: '#' },
       { name: 'Link A.5', url: '#' }
-    ],
-  },
-  {
-    header: 'Category B',
-    links: [
-      { name: 'Link B.1', url: '#' },
-      { name: 'Link B.2', url: '#' },
-      { name: 'Link B.3', url: '#' },
-      { name: 'Link B.4', url: '#' },
-      { name: 'Link B.5', url: '#' }
     ],
   },
   {
@@ -36,11 +26,21 @@ exports.sitemap = [
   {
     header: 'Category D',
     links: [
-      { name: 'Link D.1', url: '#' },
-      { name: 'Link D.2', url: '#' },
       { name: 'Link D.3', url: '#' },
       { name: 'Link D.4', url: '#' },
+      { name: 'Link D.1', url: '#' },
+      { name: 'Link D.2', url: '#' },
       { name: 'Link D.5', url: '#' }
+    ],
+  },
+  {
+    header: 'Category B',
+    links: [
+      { name: 'Link B.1', url: '#' },
+      { name: 'Link B.2', url: '#' },
+      { name: 'Link B.3', url: '#' },
+      { name: 'Link B.4', url: '#' },
+      { name: 'Link B.5', url: '#' }
     ],
   },
 ];
